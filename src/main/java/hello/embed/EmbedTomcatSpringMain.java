93b267fde7a148cb6fbc80c190bc3c331883c3cd
minu <made_power@naver.com>
2023-06-06 15:20:04 +0900

    부트 클래스 생성
    스프링 부트가 실행하는 코드들을 직접 생성해보았고 어떻게 동작하는지 확인

--- src/main/java/hello/embed/EmbedTomcatSpringMain.java
+++ src/main/java/hello/embed/EmbedTomcatSpringMain.java
@@ -12,25 +12,7 @@ public class EmbedTomcatSpringMain {
     public static void main(String[] args) throws LifecycleException {
         System.out.println("EmbedTomcatServletMain.main");
 
-        Tomcat tomcat= new Tomcat();
-        Connector connector = new Connector();
-        connector.setPort(8080);
-        tomcat.setConnector(connector);
 
-        // 스프링 컨테이너 생성
-        AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
-        appContext.register(HelloServlet.class);
-
-
-        // 디스패처 서블릿 생성
-        DispatcherServlet dispatcher = new DispatcherServlet(appContext);
-
-        //디스패처서블릿 등록
-        Context context = tomcat.addContext("","/");
-        tomcat.addServlet("","dispatcher",dispatcher);
-        context.addServletMappingDecoded("/","dispatcher");
-
-        tomcat.start();
 
     }
 
